d09d02c2a83c904c8f3e4cd58004a5752211dcd2
Vasyl Radashev <vasiliyradashev24@gmail.com>
2026-07-17 19:41:04 +0200

    feat(database): configure async PostgreSQL connection

--- app/core/config.py
+++ app/core/config.py
@@ -6,10 +6,27 @@ class Settings(BaseSettings):
     debug: bool
     version: str
 
+    postgres_host: str
+    postgres_port: int
+    postgres_db: str
+    postgres_user: str
+    postgres_password: str
+
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
     )
+
+    @property
+    def database_url(self) -> str:
+        return (
+            f"postgresql+asyncpg://"
+            f"{self.postgres_user}:"
+            f"{self.postgres_password}@"
+            f"{self.postgres_host}:"
+            f"{self.postgres_port}/"
+            f"{self.postgres_db}"
+        )
 
 
 settings = Settings()
